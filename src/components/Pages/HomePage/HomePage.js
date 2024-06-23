15ba3f1b0de89080bf1d3bae0f80df27a3c3fe27
Hristo Ivanov <gw.hivanov@gmail.com>
2024-06-23 12:12:08 +0300

    header.css added

--- src/components/Pages/HomePage/HomePage.js
+++ src/components/Pages/HomePage/HomePage.js
@@ -4,10 +4,6 @@ import Header from '../../Sections/Header/Header';
 const HomePage = () => {
   return (
     <div className="container">
-      <div className="jumbotron">
-        <h1 className="display-4">Welcome to Smart Fit Kitchen</h1>
-        <p className="lead">Healthy meals delivered to your door.</p>
-      </div>
       <Header />
     </div>
   );
